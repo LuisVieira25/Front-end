351241181041af1a9ef306e0f4c98f8103438c5d
LuisVieira25 <73907694+LuisVieira25@users.noreply.github.com>
2021-06-13 12:27:00 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <appCard />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/breeds">Breeds</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import appCard from './components/app-card.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    appCard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
